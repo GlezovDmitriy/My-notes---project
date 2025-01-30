69f8d353d70de1ac210b495ec74778a77b006e92
GlezovDmitriy <dmitriy_glezov@mail.ru>
2025-01-30 15:16:30 +0300

    Added functionality to the form

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -6,14 +6,31 @@ import FormGroup from '@mui/material/FormGroup'
 import FormLabel from '@mui/material/FormLabel'
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
-import { selectThemeMode } from '../../../../app/appSelectors'
+import {selectThemeMode} from '../../../../app/appSelectors'
 import {useAppSelector} from "common/hooks/useAppSelector";
 import {getTheme} from "common/theme/theme";
+import {Controller, SubmitHandler, useForm} from "react-hook-form";
+import s from "./Login.module.css"
 
+type Inputs = {
+    email: string
+    password: string
+    rememberMe: boolean
+}
 export const Login = () => {
     const themeMode = useAppSelector(selectThemeMode)
     const theme = getTheme(themeMode)
-
+    const {
+        control,
+        register,
+        handleSubmit,
+        reset,
+        formState: {errors},
+    } = useForm<Inputs>({defaultValues: {email: '', password: '', rememberMe: false}})
+    const onSubmit: SubmitHandler<Inputs> = data => {
+        console.log(data)
+        reset()
+    }
     return (
         <Grid container justifyContent={'center'}>
             <Grid item justifyContent={'center'}>
@@ -22,7 +39,7 @@ export const Login = () => {
                         <p>
                             To login get registered
                             <a
-                                style={{ color: theme.palette.primary.main, marginLeft: '5px' }}
+                                style={{color: theme.palette.primary.main, marginLeft: '5px'}}
                                 href={'https://social-network.samuraijs.com/'}
                                 target={'_blank'}
                                 rel="noreferrer"
@@ -38,14 +55,36 @@ export const Login = () => {
                             <b>Password:</b> free
                         </p>
                     </FormLabel>
-                    <FormGroup>
-                        <TextField label="Email" margin="normal" />
-                        <TextField type="password" label="Password" margin="normal" />
-                        <FormControlLabel label={'Remember me'} control={<Checkbox />} />
-                        <Button type={'submit'} variant={'contained'} color={'primary'}>
-                            Login
-                        </Button>
-                    </FormGroup>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <FormGroup>
+                            <TextField label="Email"
+                                       margin="normal"
+                                       {...register('email', {
+                                           required: 'Email is required',
+                                           pattern: {
+                                               value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                                               message: 'Incorrect email address',
+                                           },
+                                       })}
+                            />
+                            {errors.email && <span className={s.errorMessage}>{errors.email.message}</span>}
+                            <TextField type="password"
+                                       label="Password"
+                                       margin="normal"
+                                       {...register('password')}/>
+                            <FormControlLabel
+                                label={'Remember me'}
+                                control={<Controller
+                                    name={'rememberMe'}
+                                    control={control}
+                                    render={({field: {value, ...rest}}) => <Checkbox {...rest} checked={value}/>}
+                                />}
+                            />
+                            <Button type={'submit'} variant={'contained'} color={'primary'}>
+                                Login
+                            </Button>
+                        </FormGroup>
+                    </form>
                 </FormControl>
             </Grid>
         </Grid>
