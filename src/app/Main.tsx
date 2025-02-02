a505aac0e2ba04f4fbced5f7758dde6e664424d9
GlezovDmitriy <dmitriy_glezov@mail.ru>
2025-02-02 15:14:24 +0300

    Added navigate from Main page after Logout

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -4,9 +4,14 @@ import {AddItemForm} from "../common/components/AddItemForm";
 import {addTodolistAC, addTodolistTC} from "../features/todolists/model/todolists-reducer";
 import Todolists from "../features/ui/Todolists/Todolists";
 import {useAppDispatch} from "../common/hooks/useAppDispatch";
+import {selectIsLoggedIn} from "../features/auth/model/authSelectors";
+import {useAppSelector} from "common/hooks/useAppSelector";
+import {Navigate} from "react-router-dom";
+import {Path} from "common/router/router";
 
 export const Main = () => {
     const dispatch = useAppDispatch()
+    const isLoggedIn = useAppSelector(selectIsLoggedIn)
     const addTodolist = (title: string) => {
         dispatch(addTodolistTC(title))
         /*const action = addTodolistAC(title) // чтобы не было ошибки из-за
@@ -14,6 +19,9 @@ export const Main = () => {
         // потом можно диспатчить 2 раза.
         dispatch(action)*/ // без сервера
 
+    }
+    if (!isLoggedIn){
+        return <Navigate to={Path.Login}/>
     }
     return (
         <Container fixed>
